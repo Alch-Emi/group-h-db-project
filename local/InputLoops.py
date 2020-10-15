e322979608312982465b7ee88465735b6dc3b70c
NickAntiochos <nda7419@rit.edu>
2020-10-15 15:38:14 -0400

    Created function to tokenize input. Appears to work. Needs more testing.

---
+++ local/InputLoops.py
@@ -0,0 +1,44 @@
+"""
+@filename - InputLoops.py
+@author - Nicholas Antiochos
+File that defines all input collection/input loop logic
+"""
+
+INPUT_MSG = "Please enter a valid command\n>"
+
+def get_tokenized_input():
+    uin = input(INPUT_MSG)
+    in_chunks = []
+    dont_strip = []
+
+    start = -1
+    end = -1
+    for i in range(len(uin)):
+        if uin[i] == "'":
+            if start == -1:
+                start = i
+            else:
+
+                dont_strip.append(uin[start+1:i])
+                in_chunks.append(uin[end + 1:start])
+
+                end=i
+                start = -1
+    in_chunks.append(uin[end + 1:len(uin)])
+    tokens = []
+    for i in range(len(in_chunks)):
+        chunk = in_chunks[i].strip()
+        if len(chunk) > 0:
+            tokens += in_chunks[i].strip().split(' ')
+        if i < len(dont_strip):
+            tokens.append(dont_strip[i])
+    return tokens
+
+def main():
+    tokens = ['hello']
+    while(len(tokens) > 0):
+        tokens = get_tokenized_input()
+        print(tokens)
+
+
+main()
