ddc0988ccfe59f25b760c90140a7d33b204e8f9a
NickAntiochos <nda7419@rit.edu>
2020-10-15 19:05:44 -0400

    Began Loop logic.
    -set up dictionary for Main Loop command functions
    
    -started setting up main loop

--- local/InputLoops.py
+++ local/InputLoops.py
@@ -6,6 +6,28 @@ File that defines all input collection/input loop logic
 
 INPUT_MSG = "Please enter a valid command\n>"
 
+#Common commands
+QUIT = "quit"
+LOGOUT = 'logout'
+
+
+#Main Loop Commands
+SEARCH = "search"
+GET = "get"
+CREATE = "create"
+INVENTORY = "inventory"
+LOGOUT = "logout"
+
+mainLoopCommands = {
+    SEARCH: nothing,
+    GET: nothing,
+    CREATE: nothing,
+    INVENTORY: nothing,
+}
+
+def nothing():
+    pass
+
 def get_tokenized_input():
     uin = input(INPUT_MSG)
     in_chunks = []
@@ -34,11 +56,40 @@ def get_tokenized_input():
             tokens.append(dont_strip[i])
     return tokens
 
+
+    def LoginLoop():
+        pass
+
+    def MainLoop(recipeManager):
+        while True:
+            tokens = get_tokenized_input()
+
+            first = tokens[0]
+            if(first in mainLoopCommands):
+                mainLoopCommands[first]()
+            else:
+                print("INVALID COMMAND")
+
+    def RecipeCreateLoop():
+        pass
+
+    def RecipeListLoop():
+        pass
+
+    def RecipeViewLoop():
+        pass
+
+    def IngredientListLoop():
+        pass
+
+
 def main():
     tokens = ['hello']
     while(len(tokens) > 0):
         tokens = get_tokenized_input()
         print(tokens)
+
+
 
 
 main()
