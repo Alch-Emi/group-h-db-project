98a1a9421bd83343b35b9dcc2b6900db6b07292b
Emi Tatsuo <emi@alchemi.dev>
2020-10-18 17:18:44 -0400

    Make demo more readable, and clean out db at start

--- local/model_demo.py
+++ local/model_demo.py
@@ -1,14 +1,24 @@
 from model import RecipeManager, User
 
 if __name__ == '__main__':
-    print(dir(RecipeManager))
+    # Create RecipeManager
     man = RecipeManager.new_from_env()
+
+    # Clean out database (for demo only)
+    c = man.get_cursor()
+    c.execute("DELETE FROM users WHERE username = 'thea';")
+    c.close()
+
+    # Register new user
     user = User.register_new_user(man, 'emi', 'Secure Password Lol')
     print(f'New User: {user.uid} : {user.username} {user.pass_hash}')
+
+    # Change the user's username
     user.username = 'thea'
     user.save()
     print(f'Changed Username: {user.uid} : {user.username} {user.pass_hash}')
 
+    # Fetch the user from the database
     user = None
     user = User.get_user(man, 'thea')
     print(f'Fetched from database: {user.uid} : {user.username} {user.pass_hash}')
