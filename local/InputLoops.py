76ac2651c3fccfd9194340b52cd8d2fc04be79fa
NickAntiochos <nda7419@rit.edu>
2020-10-18 23:33:08 -0400

    Implemented signin + login/logout commands

--- local/InputLoops.py
+++ local/InputLoops.py
@@ -4,18 +4,43 @@
 File that defines all input collection/input loop logic
 """
 
-from state import State
+from recipe_manager import RecipeManager
+from user import User
+
 import help
+from state import State
 
 PROGRAM_STATE = State.MAIN #TODO change to State.LOGIN once log in functionality complete
 
+def set_program_state(state):
+    global PROGRAM_STATE
+    PROGRAM_STATE = state
+
+    print('\n' + HEADER_BORDER + ' ' + HEADER_MAP[state] + ' ' + HEADER_BORDER)
 
 INPUT_MSG = "\nEnter command ('help' for more info)\n>"
+
+#Program state headers
+HEADER_BORDER = "====="
+
+HEADER_MAP = {
+    State.LOGIN: "LOG IN",
+    State.INGREDIENT_LIST: "INGREDIENT LIST",
+    State.RECIPE_CREATE: "RECIPE CREATION",
+    State.RECIPE_LIST: "RECIPE LIST",
+    State.MAIN: "RECIPE DB"
+}
+
 
 #Common commands
 QUIT = "quit"
 LOGOUT = 'logout'
 HELP = "help"
+
+
+#Login Loop Commands
+LOGIN = "login"
+SIGNUP = "signup"
 
 
 #Main Loop Commands
@@ -30,7 +55,8 @@ INVENTORY = "inventory"
 LOGOUT = "logout"
 
 
-MANAGER = [] #TODO Create recipe manager on login
+MANAGER = RecipeManager.new_from_env()
+USER = None
 
 
 def search(tokens):
@@ -58,6 +84,30 @@ def logout(tokens):
     global LOGOUT_REQUESTED
     LOGOUT_REQUESTED = True
 
+    print(f"Farewell, {USER.username}")
+
+def login(tokens):
+    global USER
+
+    account = User.get_user(MANAGER, tokens[1])
+    if(account.check_password(tokens[2])):
+        print(f"Welcome, {tokens[1]}")
+        USER = account
+        return MainLoop()
+    else:
+        print("Invalid username or password. Please try again.")
+
+def signup(tokens):
+    account = User.register_new_user(MANAGER, tokens[1], tokens[2])
+    if(account):
+        print(f"Account successfully created. You are now signed in as {tokens[1]}")
+        USER = account
+        return MainLoop()
+    else:
+        print("Something went wrong when creating your account. Please try a different username and/or password")
+
+
+
 def getHelp(tokens):
     print(help.HELP_MESSAGE_MAP[PROGRAM_STATE])
     print(help.COMMON_HELP)
@@ -80,9 +130,14 @@ mainLoopCommands = {
     INVENTORY: nothing,
 }
 
+loginCommands = {
+    LOGIN: login,
+    SIGNUP: signup
+}
+
 COMMAND_SET_MAP = {
     State.INGREDIENT_LIST: {},
-    State.LOGIN: {},
+    State.LOGIN: loginCommands,
     State.MAIN: mainLoopCommands,
     State.RECIPE_CREATE: {},
     State.RECIPE_LIST:{},
@@ -100,7 +155,7 @@ def shouldBackOut():
     global LOGOUT_REQUESTED
     global BACK_OUT_REQUESTED
 
-    if QUIT_REQUESTED or LOGOUT_REQUESTED:
+    if QUIT_REQUESTED or (LOGOUT_REQUESTED and USER != None):
         return True
     elif BACK_OUT_REQUESTED:
         BACK_OUT_REQUESTED = False
@@ -115,7 +170,7 @@ def applyCommand(tokens):
 
     try:
         first = tokens[0]
-        if first in mainLoopCommands:
+        if first in loopCommands:
                 loopCommands[first](tokens)
         elif first in commonCommands:
                 commonCommands[first](tokens)
@@ -159,12 +214,22 @@ def get_tokenized_input():
 
 
 def LoginLoop():
-    pass
+    global PROGRAM_STATE
+    global USER
+
+
+    while not shouldBackOut():
+        set_program_state(State.LOGIN)
+        tokens = get_tokenized_input()
+        applyCommand(tokens)
+
+        USER = None # if the program returns from the deeper loop, means user was logged out
 
 def MainLoop():
     global PROGRAM_STATE
-    PROGRAM_STATE = State.MAIN
+
     while not shouldBackOut():
+        set_program_state(State.MAIN)
         tokens = get_tokenized_input()
 
         applyCommand(tokens)
@@ -183,7 +248,7 @@ def IngredientListLoop():
 
 
 def main():
-    MainLoop()
+    LoginLoop()
 
 
 if __name__ == '__main__':
